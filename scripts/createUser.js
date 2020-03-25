bb869123f714dc1f37ac5963f8113c35e1ae2b16
Gareth Harper <gareth.harper@digital.cabinet-office.gov.uk>
2020-03-25 12:07:45 +0000

    added notify to send emails when creating users

--- scripts/createUser.js
+++ scripts/createUser.js
@@ -2,6 +2,8 @@ const config = require('config');
 const authentication = require('services/authentication');
 const readline = require("readline");
 const User = require("models/user");
+const NotifyClient = require('notifications-node-client').NotifyClient;
+const randomString = require('randomstring');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,21 +11,37 @@ const rl = readline.createInterface({
 });
 
 rl.question("Enter email: ", (email) => {
-  rl.question("Enter passphrase: ", (passphrase) => {
-    authentication.hashPassphrase(passphrase).then( (hashedPassphrase) => {
-      User.create({
-        role: "user",
-        email: email,
-        hashedPassphrase: hashedPassphrase
-      });
-
-      console.log(`User ${email} created`);
-      rl.close();
-    }).catch( (err) => {
-      console.log(`Could not create user [${err}]`);
-      rl.close();
-    });
+  let passphrase = randomString.generate({
+    readable: true
   });
+  authentication.hashPassphrase(passphrase).then( (hashedPassphrase) => {
+   User.create({
+      role: "user",
+      email: email,
+      hashedPassphrase: hashedPassphrase
+    }).then( (user) => {
+      if (config.notify.apiKey) {
+        let notifyClient = new NotifyClient(config.notify.apiKey);
+        notifyClient.sendEmail(
+          config.notify.createTemplateKey,
+          email,
+          {
+            personalisation: {
+              password: passphrase
+            },
+            reference: user.id
+          },
+        );
+        console.log(`User ${email} created and email sent with password`);
+      } else {
+        console.log(`User ${email} created with password ${passphrase}`);
+      }
+    });
+
+  }).catch( (err) => {
+    console.log(`Could not create user [${err}]`);
+  });
+  rl.close();
 });
 
 
