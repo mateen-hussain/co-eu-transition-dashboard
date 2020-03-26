9ceec882167c4e3818f8186b80ccede6712939c9
Gareth Harper <gareth.harper@digital.cabinet-office.gov.uk>
2020-03-26 13:24:05 +0000

    deploy to different environments

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_SPACE=`cf target|grep space|sed 's/^space: \+\(.*$\)/\1/'`
+REQUESTED_SPACE=$1
+REQUESTED_HOST=$2
+
+if [ -z "$REQUESTED_SPACE" ] || [ -z "$REQUESTED_HOST" ]
+then
+  echo "Usage $0 <target space> <target host>"
+  exit 1
+fi
+
+cf target -s $REQUESTED_SPACE && cf push -n $REQUESTED_HOST
+RETURN=$?
+cf target -s $CURRENT_SPACE
+
+if [ $RETURN -ne 0 ]
+then
+  echo "Could not push code out check messages above"
+  exit 1
+fi
+
+
