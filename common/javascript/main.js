e9ff1c06ea539084b4cb97086c6db68f4689e5f9
Ahdab Nasir <lonan9@lonan9-m1.paconsulting.com>
2020-03-27 12:10:38 +0000

    comment explanation

--- common/javascript/main.js
+++ common/javascript/main.js
@@ -4,5 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   govukFrontend.initAll();
 });
 
+// The expanded state of individual instances of the accordion component persists across page loads using sessionStorage. 
+// These will be removed from the session storage so the accordions will collapse rather than stay open.
 sessionStorage.clear();
+
 
