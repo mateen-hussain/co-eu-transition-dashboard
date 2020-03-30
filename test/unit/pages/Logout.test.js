6afdaa8e27ec8621678803ae0e9470f0712df30d
Ahdab Nasir <lonan9@lonan9-m1.paconsulting.com>
2020-03-30 18:39:04 +0100

    unit test update

--- test/unit/pages/Logout.test.js
+++ test/unit/pages/Logout.test.js
@@ -3,6 +3,7 @@ const config = require('config');
 
 let page = {};
 let res = {}
+let req = {}
 
 describe('pages/logout/Logout', () => {
   beforeEach(() => {
@@ -10,15 +11,20 @@ describe('pages/logout/Logout', () => {
 
     res = { redirect: sinon.stub(), clearCookie: sinon.stub() };
 
-    const req = { cookies: [] };
-
     page = new Logout('some path', req, res);
-
   });
 
   describe('#url', () => {
     it('returns correct url', () => {
       expect(page.url).to.eql(config.paths.authentication.logout);
+    });
+  });
+
+  describe('#getRequest', () => {
+    it('clears the cookie and redirects', () => {
+      page.getRequest(req, res);
+      sinon.assert.calledWith(page.res.redirect, config.paths.authentication.login);
+      sinon.assert.calledWith(page.res.clearCookie, "jwt");
     });
   });
 });
