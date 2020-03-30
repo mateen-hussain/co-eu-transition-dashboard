7278d5c3d8a45fc34cfa56ebe30c884288bfd2e6
Ahdab Nasir <lonan9@lonan9-m1.paconsulting.com>
2020-03-30 16:06:25 +0100

    revert changes

--- services/authentication.js
+++ services/authentication.js
@@ -158,13 +158,6 @@ const verify2FA = (secret, token) => {
   });
 };
 
-const logout = (req, res) => {
-    // const cookie = req.cookies;
-    // res.clearCookie(config.cookie.secret, config.cookie.domain)
-    // config.cookie.secret = ''
-    // res.cookie(config.cookie.secret,"", {expires:new Date(0), domain:config.cookie.domain, path:'/'});
-  };
-
 module.exports = {
   login,
   protect,
@@ -173,5 +166,4 @@ module.exports = {
   protectNo2FA,
   verify2FA,
   hashPassphrase,
-  logout
 }
