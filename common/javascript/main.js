b5f250a97d6758d5bd99b6d4cfdde90c245179b5
Ahdab Nasir <lonan9@lonan9-m1.paconsulting.com>
2020-04-14 12:13:08 +0100

    remove spacing

--- common/javascript/main.js
+++ common/javascript/main.js
@@ -17,9 +17,3 @@ document.addEventListener('DOMContentLoaded', function() {
 // The expanded state of individual instances of the accordion component persists across page loads using sessionStorage.
 // These will be removed from the session storage so the accordions will collapse rather than stay open.
 sessionStorage.clear();
-
-
-
-
-
-
