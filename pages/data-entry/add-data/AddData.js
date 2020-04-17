6396df6af8529f85c2193eed2dcd1dca2300577e
Ahdab Nasir <ahdabnasir@gmail.com>
2020-04-17 17:16:19 +0100

    admin permission

--- pages/data-entry/add-data/AddData.js
+++ pages/data-entry/add-data/AddData.js
@@ -1,9 +1,16 @@
 const Page = require('core/pages/page');
 const { paths } = require('config');
+const authentication = require('services/authentication');
 
 class AddData extends Page {
   get url() {
     return paths.dataEntry.addData;
+  }
+
+  get middleware() {
+    return [
+      ...authentication.protect(['admin'])
+    ];
   }
 }
 
--- pages/data-entry/submission-success/SubmissionSuccess.js
+++ pages/data-entry/submission-success/SubmissionSuccess.js
@@ -1,9 +1,16 @@
 const Page = require('core/pages/page');
 const { paths } = require('config');
+const authentication = require('services/authentication');
 
 class SubmissionSuccess extends Page {
   get url() {
     return paths.dataEntry.submissionSuccess;
+  }
+
+  get middleware() {
+    return [
+      ...authentication.protect(['admin'])
+    ];
   }
 }
 
