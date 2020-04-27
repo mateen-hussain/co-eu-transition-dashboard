5b08d5231ffbde693df57b17acc4a9ba7c358646
Ahdab Nasir <ahdabnasir@gmail.com>
2020-04-27 19:03:33 +0100

    input ordering

--- common/javascript/group-display.js
+++ common/javascript/group-display.js
@@ -1,8 +1,7 @@
 export default () =>  {
 
+  // Toggle dropdown
   const dropDownOptions = document.getElementById('dropDownOptions');
-  const type = document.getElementsByName('type');
-
   function changeHandler() {
     if (this.value === 'group') {
       dropDownOptions.style.display = 'block'
@@ -11,75 +10,69 @@ export default () =>  {
     }
   }
   
+  const type = document.getElementsByName('type');
   Array.prototype.forEach.call(type, function(type) {
     type.addEventListener('change', changeHandler);
   });
 
-
+  // Add button
   const addOption = document.getElementById("add-option");
-  const dropdownOption = document.getElementsByClassName("drop-down-option");
-
   addOption.onclick = function(e) { return myHandler(e); };
 
   function myHandler() {
+    const dropdownOption = document.getElementsByClassName("drop-down-option");
 
-    var option = document.createElement('input');
+    const option = document.createElement('input');
     option.setAttribute('name', 'config[options][]');
     option.setAttribute('type', 'text');
     option.classList.add('govuk-input');
     option.classList.add('drop-down-option');
 
-    var deleteLink = document.createElement('a');
-    deleteLink.setAttribute('href', '#');
+    const deleteLink = document.createElement('a');
+    deleteLink.setAttribute('href', '#/');
     deleteLink.innerHTML = 'Delete';
     deleteLink.classList.add('govuk-link');
     deleteLink.classList.add('delete-option');
 
-    var disableLink = document.createElement('a');
-    disableLink.setAttribute('href', '#');
+    const disableLink = document.createElement('a');
+    disableLink.setAttribute('href', '#/');
     disableLink.innerHTML = 'Disable';
     disableLink.classList.add('govuk-link');
     disableLink.classList.add('disable-option');
-
+    
     deleteLink.addEventListener('click', deleteHander);
     disableLink.addEventListener('click', disableHander);
 
-    dropdownOption[0].parentNode.insertBefore(option, dropdownOption[0].nextSibling);
-    dropdownOption[0].parentNode.insertBefore(disableLink, dropdownOption[0].nextSibling);
-    dropdownOption[0].parentNode.insertBefore(deleteLink, dropdownOption[0].nextSibling);
+    dropdownOption[0].parentNode.insertBefore(option, dropdownOption[dropdownOption.length - 1].nextSibling);
+    dropdownOption[0].parentNode.insertBefore(deleteLink, dropdownOption[dropdownOption.length - 1]);
+    dropdownOption[0].parentNode.insertBefore(disableLink, dropdownOption[dropdownOption.length - 1]);
 
     return false;
   }
 
-  const deleteOption = document.getElementsByClassName('delete-option');
-
+  // Delete button
   function deleteHander() {
 
     if (this.nextElementSibling.nextElementSibling.nodeName != 'INPUT' && 
     this.previousElementSibling.previousElementSibling.nodeName != 'A') {
       return false;
     }
-
     this.previousElementSibling.remove();
     this.nextElementSibling.remove();
     this.remove();
-    return false;
   }
   
-  
+  const deleteOption = document.getElementsByClassName('delete-option');
   Array.prototype.forEach.call(deleteOption, function(deleteOption) {
     deleteOption.addEventListener('click', deleteHander);
   });
 
-  const disableOption = document.getElementsByClassName('disable-option');
-
+  // Disable button
   function disableHander() {
-
     if (this.nextElementSibling.nodeName != 'INPUT' && 
     this.previousElementSibling.previousElementSibling.previousElementSibling.nodeName != 'A') {
       return false;
     }
-
     if (this.innerHTML == 'Disable') {
       this.innerHTML = 'Enable';
       this.previousElementSibling.previousElementSibling.disabled = true;
@@ -88,7 +81,8 @@ export default () =>  {
       this.previousElementSibling.previousElementSibling.disabled = false;
     }
   }
-  
+
+  const disableOption = document.getElementsByClassName('disable-option');
   Array.prototype.forEach.call(disableOption, function(disableOption) {
     disableOption.addEventListener('click', disableHander);
   });
