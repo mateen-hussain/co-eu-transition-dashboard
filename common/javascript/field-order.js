d306a6f821986ac36d864e2b2d3c785117355dea
Ahdab Nasir <ahdabnasir@gmail.com>
2020-04-30 15:13:38 +0100

    basic ordering

---
+++ common/javascript/field-order.js
@@ -0,0 +1,51 @@
+export default () =>  {
+
+  function get_previoussibling(n){
+    let x = n.previousSibling;
+    while (x.nodeType != 1){
+      x = x.previousSibling;
+    }
+    return x;
+  } 
+    
+  function get_nextsibling(n) {
+    let x = n.nextSibling;
+    while (x != null && x.nodeType != 1) {
+      x = x.nextSibling;
+    }
+    return x;
+  }
+
+  [...document.querySelectorAll('.move-up')].forEach(function(moveUpBtn) {
+    moveUpBtn.addEventListener('click', function() {
+      let table = moveUpBtn.parentNode.parentNode.parentNode.parentNode;
+      let row = moveUpBtn.parentNode.parentNode;
+
+      while (row != null) {
+        if (row.nodeName == 'TR') {
+          break;
+        }
+        row = row.parentNode;
+      }
+      table = row.parentNode;
+      table.insertBefore(row, get_previoussibling(row));
+    });
+  });
+
+  [...document.querySelectorAll('.move-down')].forEach(function(moveDownBtn) {
+    moveDownBtn.addEventListener('click', function() {
+      let table = moveDownBtn.parentNode.parentNode.parentNode.parentNode;
+      let row = moveDownBtn.parentNode.parentNode;
+
+      while (row != null) {
+        if (row.nodeName == 'TR') {
+          break;
+        }
+        row = row.parentNode;
+      }
+      table = row.parentNode;
+      table.insertBefore (row, get_nextsibling(get_nextsibling(row)));
+    });
+  });
+
+}
