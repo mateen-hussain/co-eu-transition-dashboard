11ebf66d79112384e9b0ab9d2d7003b2a2a6762a
Gareth Harper <gareth.harper@digital.cabinet-office.gov.uk>
2020-05-13 15:43:57 +0000

    tested down and roll back if failed

--- migrations/09_completed_column.js
+++ migrations/09_completed_column.js
@@ -1,23 +1,32 @@
 const Sequelize = require('sequelize')
 
+const up = async (query) => {
+  await query.removeColumn(
+    'project',
+    'is_completed',
+  );
+};
+
+const down = async (query) => {
+  await query.addColumn(
+    'project',
+    'is_completed',
+    {
+      type: Sequelize.DataTypes.BOOLEAN,
+    }
+  );
+};
+
 module.exports = {
   up: async (query) => {
-    // logic for transforming into the new state
-    await query.removeColumn(
-      'project',
-      'is_completed',
-    );
-
+    try {
+      await up(query);
+    } catch (error) {
+      logger.error(`Error migrating ${error}`);
+      logger.error(`Rolling back changes`);
+      await down(query);
+      throw error;
+    }
   },
-
-  down: async (query) => {
-    // logic for reverting the changes
-    await query.addColumn(
-      'project',
-      'is_completed',
-      {
-        type: Sequelize.DataTypes.BOOLEAN,
-      }
-    );
-  }
+  down
 }
