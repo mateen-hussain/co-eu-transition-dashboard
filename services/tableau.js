6c5a40f8c38d17cb4f9d1158fc1aab6cf4023730
Dominic Taylor <dom.taylor7@gmail.com>
2020-07-16 10:20:35 +0100

    remove default tableau url

--- services/tableau.js
+++ services/tableau.js
@@ -2,6 +2,10 @@ const fetch = require('node-fetch');
 const config = require('config');
 
 const getTableauUrl = async (user, workbook, view) => {
+  if(!config.services.tableau.url) {
+    throw new Error('No tableau url set');
+  }
+
   const response = await fetch(`${config.services.tableau.url}/trusted`, {
     method: 'POST',
     body: {
--- test/unit/services/tableau.test.js
+++ test/unit/services/tableau.test.js
@@ -9,7 +9,28 @@ const tableau = proxyquire('services/tableau', {
 const someSecret = 'some-secret';
 
 describe('services/sequelize', () => {
+  let oldUrl;
+
+  before(() => {
+    oldUrl = config.services.tableau.url;
+  });
+
+  after(() => {
+    config.services.tableau.url = oldUrl;
+  });
+
+  it('throws if no url set in config', async () => {
+    let message = '';
+    try{
+      await tableau.getTableauUrl();
+    } catch (thrownError) {
+      message = thrownError.message;
+    }
+    expect(message).to.eql('No tableau url set');
+  });
+
   it('should create a trusted url to iframe in tableau view', async () => {
+    config.services.tableau.url = 'some url';
     nodeFetch.resolves(someSecret);
 
     const workbookName = 'some-workbook';
@@ -28,6 +49,7 @@ describe('services/sequelize', () => {
   });
 
   it('throws error if no secret returned', async () => {
+    config.services.tableau.url = 'some url';
     const user = { email: 'email@email.com' };
 
     const error = new Error('some error');
