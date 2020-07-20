3316d55ebb603d922494aa1ca1072fa2131d1838
Dominic Taylor <dom.taylor7@gmail.com>
2020-07-20 15:15:57 +0100

    log errors

--- pages/reporting-overview/ReportingOverview.js
+++ pages/reporting-overview/ReportingOverview.js
@@ -1,11 +1,14 @@
 const Page = require('core/pages/page');
 const { paths } = require('config');
 const tableau = require('services/tableau');
+const logger = require('services/logger');
+
 const getTableauUrl = async (req, res, next) => {
   try {
     res.locals.tableauIframeUrl = await tableau.getTableauUrl(this.req.user, 'MilestonesWorkbook1', 'Gantt');
   } catch (error) {
     res.locals.tableauIframeError = error;
+    logger.error(`Error from tableau: ${error}`);
   }
   next();
 };
