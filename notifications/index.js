ef3850d83413f94b1ffbaed88aaa71d6ca635185
Dominic Taylor <dom.taylor7@gmail.com>
2020-08-06 15:15:52 +0100

    logs

--- notifications/index.js
+++ notifications/index.js
@@ -3,5 +3,5 @@ const upcomingMilestones = require('./upcomingMilestones');
 const logger = require('services/logger');
 
 // notify users of upcoming milestones at 10 am each day
-cron.schedule('0 0 10 * * *', upcomingMilestones.notifyUpcomingMilestones);
+cron.schedule('0 10 * * * *', upcomingMilestones.notifyUpcomingMilestones);
 logger.info(`Missed milestone notifications active`);
