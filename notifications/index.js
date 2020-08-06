c1167f48622c9f6d18cc23ce68a3f3503f6bab70
Dominic Taylor <dom.taylor7@gmail.com>
2020-08-06 15:40:01 +0100

    cron settings to cinfig

--- notifications/index.js
+++ notifications/index.js
@@ -1,7 +1,8 @@
 const cron = require('node-cron');
 const upcomingMilestones = require('./upcomingMilestones');
 const logger = require('services/logger');
+const config = require('config');
 
 // notify users of upcoming milestones at 10 am each day
-cron.schedule('0 10 * * * *', upcomingMilestones.notifyUpcomingMilestones);
+cron.schedule(config.notify.cron, upcomingMilestones.notifyUpcomingMilestones);
 logger.info(`Missed milestone notifications active`);
