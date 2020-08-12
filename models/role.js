4b39edaa3dc3fd47ba8889428b8cf5abb84302c1
“Ahdab <ahdab.nasir@paconsulting.com>
2020-08-12 17:45:45 +0100

    user role relationships

--- models/role.js
+++ models/role.js
@@ -1,5 +1,6 @@
 const { Model, INTEGER, ENUM } = require('sequelize');
 const sequelize = require('services/sequelize');
+const UserRole = require('./userRole');
 
 class Role extends Model {}
 Role.init({
@@ -11,5 +12,8 @@ Role.init({
     type: ENUM('uploader', 'viewer', 'admin', 'management', 'guest')
   }
 }, { sequelize, modelName: 'role', tableName: 'role', timestamps: false });
+
+Role.hasMany(UserRole, { foreignKey: 'role_id' });
+UserRole.belongsTo(Role, { foreignKey: 'role_id' });
 
 module.exports = Role;
