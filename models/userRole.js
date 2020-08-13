6914b93ef585df20776c3d4da5dcd03442703f95
“Ahdab <ahdab.nasir@paconsulting.com>
2020-08-13 14:28:16 +0100

    basic model test

--- models/userRole.js
+++ models/userRole.js
@@ -1,7 +1,15 @@
 const { Model, INTEGER } = require('sequelize');
 const sequelize = require('services/sequelize');
 
-class UserRole extends Model {}
+class UserRole extends Model {
+  static async getUserRole(userId) {
+    const roles = await UserRole.findOne({
+      where: { user_id: userId }
+    });
+    console.log(roles);
+  }
+}
+
 UserRole.init({
   user_id: {
     type: INTEGER
