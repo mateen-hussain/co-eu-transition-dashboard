16b3aaeb2a1691d45e9621fd8b7ccc57df8e6212
“Ahdab <ahdab.nasir@paconsulting.com>
2020-09-01 11:22:54 +0100

    query update

--- migrations/14_convert_user_roles.js
+++ migrations/14_convert_user_roles.js
@@ -5,8 +5,7 @@ module.exports = {
     const transaction = await query.sequelize.transaction();
     try {
       await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'viewer' AND r.name = 'viewer'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
-      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'uploader' AND r.name = 'viewer'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
-      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'uploader' AND r.name = 'uploader'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
+      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'uploader' AND r.name IN ('uploader', 'viewer')", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
       await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'administrator'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
 
       await transaction.commit();
