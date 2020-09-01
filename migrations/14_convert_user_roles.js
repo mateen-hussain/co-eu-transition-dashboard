6c85864dca6a0af9bb439e3a369b113ecd2312d0
“Ahdab <ahdab.nasir@paconsulting.com>
2020-09-01 10:56:28 +0100

    uploader role update

--- migrations/14_convert_user_roles.js
+++ migrations/14_convert_user_roles.js
@@ -4,9 +4,10 @@ module.exports = {
   up: async (query) => {
     const transaction = await query.sequelize.transaction();
     try {
-      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user as u, role as r WHERE u.role = 'viewer' and r.name = 'viewer'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
-      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user as u, role as r WHERE u.role = 'uploader' and r.name = 'uploader'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
-      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user as u, role as r WHERE u.role = 'administrator'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
+      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'viewer' AND r.name = 'viewer'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
+      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'uploader' AND r.name = 'viewer'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
+      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'uploader' AND r.name = 'uploader'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
+      await query.sequelize.query("INSERT INTO user_role(user_id, role_id) SELECT u.id, r.id FROM user AS u, role AS r WHERE u.role = 'administrator'", { type: query.sequelize.QueryTypes.INSERT, transaction, raw: true });
 
       await transaction.commit();
     } catch (error) {
