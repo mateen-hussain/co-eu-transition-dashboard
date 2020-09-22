2a6e98485264b08fd02116521e76a1783e4092ad
Dominic Taylor <dom.taylor7@gmail.com>
2020-09-22 17:28:18 +0100

    add rest of themes

--- pages/readiness-overview/ReadinessOverview.js
+++ pages/readiness-overview/ReadinessOverview.js
@@ -28,54 +28,12 @@ class ReadinessOverview extends Page {
       entity.active = true;
     });
 
-    const themesWithStubs = [...this.stubThemes, ...data.allThemes]
-
-    const themesGrouped = groupBy(themesWithStubs, theme => theme.color);
+    const themesGrouped = groupBy(data.allThemes, theme => theme.color);
 
     return {
       headlineMeasures: data.headlineEntites,
       themes: themesGrouped
     };
-  }
-
-  get stubThemes() {
-    return [
-      {
-        name: "economy",
-        color:  "amber",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "Data",
-        color:  "amber",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "Energy / Environment",
-        color:  "yellow",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "Animals & food",
-        color:  "amber",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "Fisheries",
-        color:  "amber",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "Security",
-        color:  "yellow",
-        description: "Theme due online: 30/09/2020"
-      },
-      {
-        name: "International agreements",
-        color:  "yellow",
-        description: "Theme due online: 30/09/2020"
-      }
-    ];
   }
 }
 
