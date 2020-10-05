8e81d017784a4cd3b5694980e52d8377c6dc5d6c
“Ahdab <ahdab.nasir@paconsulting.com>
2020-10-05 15:52:26 +0100

    table sorting and heading size change

--- common/javascript/sort-table.js
+++ common/javascript/sort-table.js
@@ -4,15 +4,16 @@ export default () =>  {
   const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
   const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    v2 !== '' && v1 !== '' && !isNaN(v2) && !isNaN(v1) ? v2 - v1 : v2.toString().localeCompare(v1)
   )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
     
   document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    Array.from(document.getElementsByClassName('sort-coloumn')).forEach(th => th.addEventListener('click', (() => {
       const table = th.closest('table');
       Array.from(table.querySelectorAll('tbody'))
         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tbody => table.appendChild(tbody) );
+        .forEach(tbody => table.appendChild(tbody));
+      th.classList.toggle('down-sort');
     })));
   })
 }
