a834bb8e1abc811caca1f6045264dcedb669ce30
ahdabnasir@gmail.com <ahdab.nasir@paconsulting.com>
2020-11-02 12:59:24 +0000

    hide filter option and date fix

--- common/javascript/sort-table.js
+++ common/javascript/sort-table.js
@@ -4,8 +4,8 @@ export default () =>  {
   const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
   const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v2 !== '' && v1 !== '' && !isNaN(v2) && !isNaN(v1) ? v2 - v1 : v2.toString().localeCompare(v1)
-  )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+    v1 !== '' && v1 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+  )(getCellValue(asc ? a : b, idx));
     
   document.addEventListener('DOMContentLoaded', function() {
     Array.from(document.getElementsByClassName('sort-coloumn')).forEach(th => th.addEventListener('click', (() => {
