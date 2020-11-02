dfa6f9974fb2f2fe494daada967a3d85ff4bf491
ahdabnasir@gmail.com <ahdab.nasir@paconsulting.com>
2020-11-02 15:52:50 +0000

    number fix

--- common/javascript/sort-table.js
+++ common/javascript/sort-table.js
@@ -4,7 +4,7 @@ export default () =>  {
   const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
   const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v1 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
   )(getCellValue(asc ? a : b, idx));
     
   document.addEventListener('DOMContentLoaded', function() {
